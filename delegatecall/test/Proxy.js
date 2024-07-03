027d5e9e52212a90ce614c98e887c69281fc4e09
jakkie <jakubkielbasiewicz@gmail.com>
2024-07-03 23:05:20 +0200

    proxy test init

--- delegatecall/test/Proxy.js
+++ delegatecall/test/Proxy.js
@@ -2,5 +2,8 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe('Proxy test', () => {
-
+    let owner;
+    beforeEach(async () => {
+        owner = await ethers.getSigners();
+    })
 })
