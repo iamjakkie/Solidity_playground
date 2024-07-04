6d8d820b5f2e8f17c3edee0b3f21576571b023ca
jakkie <jakubkielbasiewicz@gmail.com>
2024-07-04 12:45:23 +0200

    proxy tests

--- delegatecall/test/Proxy.js
+++ delegatecall/test/Proxy.js
@@ -5,5 +5,17 @@ describe('Proxy test', () => {
     let owner;
     beforeEach(async () => {
         owner = await ethers.getSigners();
+
+        const Library = await ethers.getContractFactory('Library');
+        const library = await Library.deploy();
+        await library.deployed();
+
+        const Proxy = await ethers.getContractFactory('Proxy');
+        const proxy = await Proxy.deploy(library.address);
+        await proxy.deployed();
     })
+
+    describe('delegatecall', () => {
+        it('')
+    });
 })
