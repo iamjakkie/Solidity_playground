1c4763ef7b5ff4858be240deb4cae5a2f4708ef3
jakkie <jakubkielbasiewicz@gmail.com>
2024-07-14 00:02:04 +0200

    bit masking

--- evm_storage/contracts/EVMStorage.sol
+++ evm_storage/contracts/EVMStorage.sol
@@ -11,7 +11,7 @@ contract EVMStorageSingleSlot {
     uint256 public s_y; // slot 1
     bytes32 public s_z; // slot 2
 
-    function test_sstore() public {
+    function sstore() public {
         assembly {
             sstore(0, 111)
             sstore(1, 222)
@@ -19,7 +19,7 @@ contract EVMStorageSingleSlot {
         }
     }
 
-    function test_sstore_new() public {
+    function sstore_new() public {
         assembly {
             sstore(s_x.slot, 123)
             sstore(s_y.slot, 456)
@@ -27,7 +27,7 @@ contract EVMStorageSingleSlot {
         }
     }
 
-    function test_sload() public view returns (uint256 x, uint256 y, bytes32 z){
+    function sload() public view returns (uint256 x, uint256 y, bytes32 z){
         assembly {
             x := sload(0)
             y := sload(1)
@@ -35,11 +35,56 @@ contract EVMStorageSingleSlot {
         }
     }
 
-    function test_sload_new() public view returns (uint256 x, uint256 y, bytes32 z){
+    function sload_new() public view returns (uint256 x, uint256 y, bytes32 z){
         assembly {
             x := sload(s_x.slot)
             y := sload(s_y.slot)
             z := sload(s_z.slot)
+        }
+    }
+}
+
+contract BitMasking {
+    
+}
+
+contract EVMStoragePackedSlotBytes {
+    // slot 0 (packed right to left)
+    // 0x000000 ... 00000000
+    
+    //0x000000...abababab
+    bytes4 public b4 = 0xabababab;
+    //0x000000..cdcdabababab
+    bytes2 public b2 = 0xcdcd;
+    // 0x0000000000000000000000000000000000000000000000000000cdcdabababab
+    //
+}
+
+contract EVMStoragePackedSlot {
+    // Data < 32 bytes are packed into a slot
+    // Bit masking
+    // slot, offset
+
+    // slot0
+    uint128 public s_a;
+    uint64 public s_b;
+    uint32 public s_c;
+    uint32 public s_d;
+
+    // slot1
+    address public s_addr; // 20 bytes = 160bits
+    // 96 bits
+    uint64 public s_x;
+    uint32 public s_y;
+
+    function sstore() public {
+        assembly {
+            let v:= sload(0)
+
+            // s_d | s_c | s_b | s_a
+            // 32  | 32  | 64  | 128
+
+            sstore(0, v)
         }
     }
 }
