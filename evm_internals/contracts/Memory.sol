003bb405cb378c887f190e7579b7b0fccf85eca6
jakkie <jakubkielbasiewicz@gmail.com>
2024-07-24 16:42:37 +0200

    Gas costs calculation

--- evm_internals/contracts/Memory.sol
+++ evm_internals/contracts/Memory.sol
@@ -12,3 +12,12 @@ contract MemBasic {
         }
     }
 }
+
+contract MemExp {
+    function alloc_mem(uint256 n) external view returns (uint256) {
+        uint256 gas_start = gasleft();
+        uint256[] memory arr = new uint256[](n);
+        uint256 gas_end = gasleft();
+        return gas_start - gas_end;
+    }
+}
--- evm_internals/test/Memory.js
+++ evm_internals/test/Memory.js
@@ -15,4 +15,21 @@ describe('EVM Memory tests', () => {
             expect(b32).to.equal("0x0000000000000000000000000000000000000000000000000000000000ababab");
         })
     })
+
+    describe('Gas costs', () => {
+        let exp
+
+        beforeEach(async () => {
+            const Exp = await ethers.getContractFactory('MemExp');
+            exp = await Exp.deploy();
+        });
+
+        it('Gas costs', async () => {
+            const gas_used_0 = await exp.alloc_mem(0);
+            expect(gas_used_0).to.equal(122);
+
+            const gas_used_1 = await exp.alloc_mem(1);
+            expect(gas_used_1).to.equal(180);
+        })
+    })
 })
